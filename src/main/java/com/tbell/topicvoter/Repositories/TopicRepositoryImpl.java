db60d10de42ed59152ef95ceab29ca5ec57f64af
tkbell51 <tkbell51@gmail.com>
2017-07-31 11:15:05 -0400

    Database integration for findAll complete and shows on html

--- src/main/java/com/tbell/topicvoter/Repositories/TopicRepositoryImpl.java
+++ src/main/java/com/tbell/topicvoter/Repositories/TopicRepositoryImpl.java
@@ -4,8 +4,11 @@ import com.tbell.topicvoter.interfaces.TopicRepository;
 import com.tbell.topicvoter.models.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -15,10 +18,18 @@ public class TopicRepositoryImpl implements TopicRepository{
     JdbcTemplate jdbcTemplate;
 
 
-
-
     public List<Topic> findAll() {
-        jdbcTemplate.query("SELECT * FROM topic");
-        return null;
+        return jdbcTemplate.query("SELECT * FROM topic", new TopicMapper());
+
+    }
+    private static class TopicMapper implements RowMapper<Topic> {
+
+        @Override
+        public Topic mapRow(ResultSet resultSet, int i) throws SQLException {
+            Topic topic = new Topic(resultSet.getInt("id"),
+                    resultSet.getString("title"),
+                    resultSet.getString("description"));
+            return topic;
+        }
     }
 }
--- src/main/java/com/tbell/topicvoter/controllers/TopicController.java
+++ src/main/java/com/tbell/topicvoter/controllers/TopicController.java
@@ -1,26 +1,25 @@
 package com.tbell.topicvoter.controllers;
 
+import com.tbell.topicvoter.Repositories.TopicRepositoryImpl;
 import com.tbell.topicvoter.models.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TopicController {
 
+    @Autowired
+    private TopicRepositoryImpl repo;
+
     @RequestMapping("/")
     public String index(Model model){
-        Topic first = new Topic(1, "Java Spring", "Not sure how spring works");
-        Topic second = new Topic(2, "Backend Fundamentals", "NODE ON THE BACKEND?! WHAT!");
-        Topic third = new Topic(3, "React & Redux", "What is a single page app anyway?");
-        ArrayList<Topic> topics = new ArrayList<>();
-        topics.add(first);
-        topics.add(second);
-        topics.add(third);
-
-        model.addAttribute("topics", topics);
+        List<Topic> allTopics = repo.findAll();
+        model.addAttribute("topics",allTopics);
         return "index";
     }
 }
